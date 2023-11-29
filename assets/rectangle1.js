a413c89d7d43e1bc153b0dd9127a7e820b58691f
brian890809 <brian08090809@gmail.com>
2023-11-28 22:52:28 -0800

    applied image clipping

--- assets/rectangle1.js
+++ assets/rectangle1.js
@@ -1,30 +1,34 @@
-import Image from 'next/image';
-
-import { PicturePageBase } from '@/components/PicturePageBase';
-
-// import styles from './rectanble1.css';
-
 const Rectangle1 = ({ imgSrc, customStlyes, id, ...props }) => {
-  const { innerWidth: vWidth, innerHeight: vHeight } = window;
-  const scaleX = vWidth / 393;
-  const scaleY = (vHeight * 0.8) / 796;
-
   return (
     <svg
       id={id}
-      className="relative m-auto"
-      width="393"
-      height="850"
+      className={`relative m-auto ${customStlyes}`}
+      width="100%"
+      height="80vh"
       viewBox="0 0 393 796"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      transform={`scale(${scaleX} ${scaleY}) `}
+      preserveAspectRatio="none"
+      {...props}
     >
-      <path
-        class="path-anim"
-        d="M 0 8 C 174 8 213 8 393 8 V 758 C 288 758 215 758 0 758 V 8 Z"
-        data-path-to="M 0 8 C 142 513 299 -188 393 8 V 758 C 307 873 224 942 0 758 V 8 Z"
-        fill="#D9D9D9"
+      <defs>
+        <clipPath id="shape1">
+          <path
+            class="path-anim"
+            d="M 0 8 C 174 8 213 8 393 8 V 758 C 288 758 215 758 0 758 V 8 Z"
+            data-path-to="M 0 8 C 142 513 299 -188 393 8 V 758 C 307 873 224 942 0 758 V 8 Z"
+            fill="#D9D9D9"
+          />
+        </clipPath>
+      </defs>
+      <image
+        clip-path="url(#shape1)"
+        href={imgSrc}
+        x="0"
+        y="0"
+        width="100%"
+        height="100%"
+        preserveAspectRatio="xMidYMid slice"
       />
     </svg>
   );
